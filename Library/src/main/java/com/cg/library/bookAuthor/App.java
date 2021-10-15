f94cd71c546e82454280713a75339c3c2f8d8e29
paithriv <paithriv@LIN33003410.corp.capgemini.com>
2021-10-15 15:45:41 +0530

    my commit 6

--- Library/src/main/java/com/cg/library/bookAuthor/App.java
+++ Library/src/main/java/com/cg/library/bookAuthor/App.java
@@ -7,7 +7,6 @@ import com.cg.library.services.IAuthorServices;
 public class App {
   public static void main(String[] args) {
 	  
-	  
 	  IAuthorServices ia=new AuthorServicesImpl();
 	  boolean b=ia.storeAuthorData(new Author("Lee","Min","Hoo",231456874));
 	  if(b)   System.out.println("Successfully added !");
